51d9bef2030264f01690b457b2954703434d8485
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2020-03-02 14:06:49 -0500

    Undo JSDOM import.

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -2,7 +2,9 @@ import React, { Fragment } from "react";
 import { renderToString } from "react-dom/server";
 import { Minimatch } from "minimatch";
 import flattenDeep from "lodash.flattendeep";
-const {JSDOM} = require("jsdom");
+const JSDOM = eval('require("jsdom")').JSDOM;
+// In development, try this to get better types.
+// const {JSDOM} = require("jsdom");
 const minimatch = require("minimatch");
 
 const ampBoilerplate = `body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}`;
